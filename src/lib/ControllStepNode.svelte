c824a483972f56f830033787784268572f984e08
Sasha Weir <sashasalzweir@gmail.com>
2025-07-26 00:34:04 +0200

    refactored out the edit button svg

--- src/lib/ControllStepNode.svelte
+++ src/lib/ControllStepNode.svelte
@@ -2,6 +2,7 @@
     import { Handle, Position, type NodeProps } from "@xyflow/svelte";
     let { data }: NodeProps = $props();
     import { stepEditor } from "./processState.svelte";
+    import EditButton from "./UIComponents/editButton.svelte";
     import ProcessStepEditor from "./processStepEditor.svelte";
     const onClickEdit = ({ event }) => {
         stepEditor.currentStepID = data.id;
@@ -20,38 +21,7 @@
     <div>
         {data.condition}
     </div>
-    <button class="nodrag" onclick={onClickEdit}>
-        <svg
-            width="15px"
-            height="15px"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <g id="Complete">
-                <g id="edit">
-                    <g>
-                        <path
-                            d="M20,16v4a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V6A2,2,0,0,1,4,4H8"
-                            fill="none"
-                            stroke="#000000"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                        />
-
-                        <polygon
-                            fill="none"
-                            points="12.5 15.8 22 6.2 17.8 2 8.3 11.5 8 16 12.5 15.8"
-                            stroke="#000000"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                        />
-                    </g>
-                </g>
-            </g>
-        </svg>
-    </button>
+    <EditButton classProp="nodrag" onclick={onClickEdit}></EditButton>
 </div>
 <Handle
     id="pos"
